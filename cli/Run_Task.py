1e92e751899e16eb417f2a26c87f443d8df1e122
madhavpai09 <madhav09pai@gmail.com>
2026-01-13 16:39:06 +0530

    Added cli for running

--- cli/Run_Task.py
+++ cli/Run_Task.py
@@ -1,12 +1,14 @@
-import click # type: ignore
-import importlib
+import click  # try: ignore
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent / "Tasks"))
 
 @click.command("run_task")
 @click.option('--task_name', prompt='Task Name', help='Name of the task to run.')
-def run_task(task_name):
-    module_path = f'{task_name}.main'
-    module = importlib.import_module(module_path)
-    module.main(task_name)
+def run_task(task_name, **kwargs):
+    task_module = __import__(f'{task_name}.main', fromlist=['main'])
+    task_module.main(task_name, **kwargs)
 
 if __name__ == '__main__':
     run_task()
